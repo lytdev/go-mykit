a799bfc285cd18eebef66d253194c5e0b8c27cf8
刘元涛 <snoopy_718@mails.ccnu.edu.cn>
2022-10-14 19:17:17 +0800

    结构体列表写入到文件测试

--- gme/excel_test.go
+++ gme/excel_test.go
@@ -12,14 +12,14 @@ import (
 )
 
 type ExcelTest struct {
-	Isbn      string    `gme:"title:ISBN;index:0"`
-	BookName  string    `gme:"title:书名;index:1"`
+	Isbn      string    `gme:"title:ISBN;index:1"`
+	BookName  string    `gme:"title:书名;index:0"`
 	Author    string    `gme:"title:作者;index:2"`
-	PubDate   time.Time `gme:"title:出版日期;index:3"`
-	Price     float32   `gme:"title:定价;index:4"`
+	PubDate   time.Time `gme:"title:出版日期;index:4;format:datetime"`
+	Price     float32   `gme:"title:定价;index:3"`
 	SuitObj   string    `gme:"title:适用对象;index:5"`
 	MajorType string    `gme:"title:图书类目;index:6"`
-	SubMajor  string    `gme:"title:细分类目;index:7"`
+	SubMajor  string    `gme:"title:细分类目;index:7";`
 }
 
 func TestRead(t *testing.T) {
@@ -54,5 +54,50 @@ func TestRead(t *testing.T) {
 	for _, data := range resultData {
 		fmt.Println(data)
 		t.Log(data)
+	}
+}
+
+func TestWriteFile(t *testing.T) {
+	pubDate, err := time.ParseInLocation(DATE_PATTERN, "2021-12-01", time.Local)
+	if err != nil {
+		fmt.Println(err)
+	}
+	var dataList = []ExcelTest{}
+	dataList = append(dataList, ExcelTest{
+		Isbn:      "9787115375698",
+		BookName:  "Excel 2013在会计与财务管理日常工作中的应用",
+		Author:    "神龙工作室 编著",
+		PubDate:   time.Now(),
+		Price:     49.8,
+		SuitObj:   "专科",
+		MajorType: "计算机类",
+		SubMajor:  "办公软件"})
+	dataList = append(dataList, ExcelTest{
+		Isbn:      "9787115383433",
+		BookName:  "Excel 2013数据处理与分析",
+		Author:    "金桥  周奎奎",
+		PubDate:   pubDate,
+		Price:     69,
+		SuitObj:   "专科",
+		MajorType: "计算机类",
+		SubMajor:  "办公软件"})
+	dataList = append(dataList, ExcelTest{
+		Isbn:      "9787115512819",
+		BookName:  "中文版Rhino 5.0实用教程",
+		Author:    "邓宇燕",
+		PubDate:   pubDate,
+		Price:     59.9,
+		SuitObj:   "专科",
+		MajorType: "计算机类",
+		SubMajor:  "三维设计"})
+	sheetName := "Sheet1"
+	f, err := WriteFile(sheetName, dataList)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	// 根据指定路径保存文件
+	if err := f.SaveAs("../_doc/测试excel输出.xlsx"); err != nil {
+		fmt.Println(err)
 	}
 }
