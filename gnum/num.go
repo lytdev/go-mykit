5fe255511624a65be10b22e33ff0f026dafe5f8d
lytdev <lytdev@outlook.com>
2022-11-28 21:33:38 +0800

    添加生成id工具

--- gnum/num.go
+++ gnum/num.go
@@ -1,9 +1,27 @@
 package gnum
 
 import (
+	"crypto/rand"
 	"encoding/json"
+	"math/big"
 	"strconv"
 )
+
+// RandomInt 随机数
+func RandomInt(min, max int64) int64 {
+	// calculate the max we will be using
+	bg := big.NewInt(max - min)
+
+	// get big.Int between 0 and bg
+	// in this case 0 to 20
+	n, err := rand.Int(rand.Reader, bg)
+	if err != nil {
+		panic(err)
+	}
+
+	// add n to min to support the passed in range
+	return n.Int64() + min
+}
 
 // AnyToInt 任意数据转int
 func AnyToInt(value interface{}) int {
--- gstr/str.go
+++ gstr/str.go
@@ -1,6 +1,10 @@
 package gstr
 
-import "unsafe"
+import (
+	"crypto/rand"
+	"strings"
+	"unsafe"
+)
 
 //string与[]byte的直接转换是通过底层数据copy实现的,存在性能损耗,不过一般不影响
 
@@ -14,4 +18,45 @@ func StrToByteArr(s string) []byte {
 // ByteArrToStr 字节数组转字符串
 func ByteArrToStr(b []byte) string {
 	return *(*string)(unsafe.Pointer(&b))
+}
+
+// IsEmpty 是否为空
+func IsEmpty(val string) bool {
+	s := strings.TrimSpace(val)
+	return len(s) == 0
+}
+
+const (
+	LettersLetter          = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	LettersUpperCaseLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	LettersNumber          = "0123456789"
+	LettersNumberNoZero    = "23456789"
+	LettersSymbol          = "~`!@#$%^&*()_-+={[}]|\\:;\"'<,>.?/"
+)
+
+// RandString 随机字符串
+func RandString(n int, letters ...string) (string, error) {
+
+	lettersDefaultValue := LettersLetter + LettersNumber + LettersSymbol
+
+	if len(letters) > 0 {
+		lettersDefaultValue = ""
+		for _, letter := range letters {
+			lettersDefaultValue = lettersDefaultValue + letter
+		}
+	}
+
+	bytes := make([]byte, n)
+
+	_, err := rand.Read(bytes)
+
+	if err != nil {
+		return "", err
+	}
+
+	for i, b := range bytes {
+		bytes[i] = lettersDefaultValue[b%byte(len(lettersDefaultValue))]
+	}
+
+	return string(bytes), nil
 }
