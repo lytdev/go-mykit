36947b0676f1eb883d54dd2eaabaf83d751226dd
lytdev <lytdev@outlook.com>
2022-11-29 12:32:37 +0800

    优化结构目录

--- gconv/gconv.go
+++ gconv/gconv.go
@@ -1,6 +1,10 @@
 package gconv
 
-import "strings"
+import (
+	"encoding/json"
+	"strconv"
+	"strings"
+)
 
 // TagConvMap /** 结构体的tag(tag的多个kay-value是使用;隔开的)转map
 func TagConvMap(tagStr string) map[string]string {
@@ -13,4 +17,59 @@ func TagConvMap(tagStr string) map[string]string {
 		}
 	}
 	return resultData
+}
+
+// AnyToInt 任意数据转int
+func AnyToInt(value interface{}) int {
+	if value == nil {
+		return 0
+	}
+	switch val := value.(type) {
+	case int:
+		return val
+	case int8:
+		return int(val)
+	case int16:
+		return int(val)
+	case int32:
+		return int(val)
+	case int64:
+		return int(val)
+	case uint:
+		return int(val)
+	case uint8:
+		return int(val)
+	case uint16:
+		return int(val)
+	case uint32:
+		return int(val)
+	case uint64:
+		return int(val)
+	case *string:
+		v, err := strconv.Atoi(*val)
+		if err != nil {
+			return 0
+		}
+		return v
+	case string:
+		v, err := strconv.Atoi(val)
+		if err != nil {
+			return 0
+		}
+		return v
+	case float32:
+		return int(val)
+	case float64:
+		return int(val)
+	case bool:
+		if val {
+			return 1
+		} else {
+			return 0
+		}
+	case json.Number:
+		v, _ := val.Int64()
+		return int(v)
+	}
+	return 0
 }
