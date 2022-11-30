ac29dafdc8a837e3ebef98bfd319176baf9f2e94
刘元涛 <snoopy_718@mails.ccnu.edu.cn>
2022-11-30 15:22:19 +0800

    添加数字位数不够补0的方法

--- gnum/num.go
+++ gnum/num.go
@@ -1,1 +1,17 @@
 package gnum
+
+import (
+	"strconv"
+	"strings"
+)
+
+// NumFillZero 数字转字符串,位数不够的前面补0
+func NumFillZero(n, l int) string {
+	numStr := strconv.Itoa(n)
+	sb := strings.Builder{}
+	for i := 0; i < (l - len(numStr)); i++ {
+		sb.WriteString("0")
+	}
+	sb.WriteString(numStr)
+	return sb.String()
+}
