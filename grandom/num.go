73d27cd2c82e49f07b89d503807a4bffbc6b7441
刘元涛 <snoopy_718@mails.ccnu.edu.cn>
2022-11-30 10:54:15 +0800

    完善随机数生成

--- grandom/num.go
+++ grandom/num.go
@@ -5,18 +5,39 @@ import (
 	"time"
 )
 
-// RandomInt 生成随机整数
-func RandomInt(min, max int) int {
-	rand.Seed(time.Now().Unix()) //随机种子
+func init() {
+	//随机种子
+	rand.Seed(time.Now().UnixNano())
+}
+
+// RandInt 生成随机整数
+func RandInt(n int) int {
+	if n <= 0 {
+		return 0
+	}
+	return rand.Intn(n)
+}
+
+// RandomIntRange 生成一定范围内的随机整数
+func RandomIntRange(min, max int) int {
 	return rand.Intn(max-min) + min
 }
 
-// RandomFloat 生成随机浮点数
-func RandomFloat(min, max float64) float64 {
+// RandomFloatRange 生成一定范围内的随机浮点数
+func RandomFloatRange(min, max float64) float64 {
 	if min > max {
 		min, max = max, min
 	}
 	// 到这里确保 max>=min 并且二者一定是正数
 	ret := min + rand.Float64()*(max-min)
 	return ret
+}
+
+// RandIntSlice 生成随机整数切片
+func RandIntSlice(l int, max int) []int {
+	slice := make([]int, l)
+	for i := 0; i < l; i++ {
+		slice[i] = RandInt(max)
+	}
+	return slice
 }
--- grandom/str.go
+++ grandom/str.go
@@ -2,64 +2,59 @@ package grandom
 
 import (
 	"math/rand"
-	"strconv"
 	"strings"
-	"time"
 )
-
-// RandomString 生成随机字符串
-func RandomString(length int) string {
-	// 48 ~ 57 数字
-	// 65 ~ 90 A ~ Z
-	// 97 ~ 122 a ~ z
-	// 一共62个字符，在0~61进行随机，小于10时，在数字范围随机，
-	// 小于36在大写范围内随机，其他在小写范围随机
-	rand.Seed(time.Now().UnixNano())
-	result := make([]string, 0, length)
-	for i := 0; i < length; i++ {
-		t := rand.Intn(62)
-		if t < 10 {
-			result = append(result, strconv.Itoa(t))
-		} else if t < 36 {
-			result = append(result, string(rune(rand.Intn(26)+65)))
-		} else {
-			result = append(result, string(rune(rand.Intn(26)+97)))
-		}
-	}
-	return strings.Join(result, "")
-}
 
 const (
-	LettersLetter          = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	LettersUpperCaseLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	LettersNumber          = "0123456789"
-	LettersNumberNoZero    = "23456789"
-	LettersSymbol          = "~`!@#$%^&*()_-+={[}]|\\:;\"'<,>.?/"
+	lettersLetter          = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	enAlphabetLen          = len(lettersLetter)
+	lettersUpperCaseLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	lowerEnLetter          = "abcdefghijklmnopqrstuvwxyz"
+	lowerEnLetterLen       = len(lowerEnLetter)
+	lettersNumber          = "0123456789"
+	lettersNumberNoZero    = "23456789"
+	lettersSymbol          = "~`!@#$%^&*()_-+={[}]|\\:;\"'<,>.?/"
 )
 
-// RandString 随机字符串
-func RandString(n int, letters ...string) (string, error) {
-
-	lettersDefaultValue := LettersLetter + LettersNumber + LettersSymbol
-
+// RandomString 随机字符串
+func RandomString(n int, letters ...string) (string, error) {
+	lettersDefaultValue := lettersLetter + lettersNumber + lettersSymbol
 	if len(letters) > 0 {
 		lettersDefaultValue = ""
 		for _, letter := range letters {
 			lettersDefaultValue = lettersDefaultValue + letter
 		}
 	}
-
 	bytes := make([]byte, n)
-
 	_, err := rand.Read(bytes)
-
 	if err != nil {
 		return "", err
 	}
-
 	for i, b := range bytes {
 		bytes[i] = lettersDefaultValue[b%byte(len(lettersDefaultValue))]
 	}
-
 	return string(bytes), nil
+}
+
+// RandomStr 随机字符串
+func RandomStr(l uint64) string {
+	b := make([]byte, l)
+	for i := range b {
+		b[i] = lettersLetter[RandInt(enAlphabetLen)]
+	}
+	return string(b)
+}
+
+// RandomLowerStr 随机字符串小写
+func RandomLowerStr(l uint32) string {
+	b := make([]byte, l)
+	for i := range b {
+		b[i] = lowerEnLetter[RandInt(lowerEnLetterLen)]
+	}
+	return string(b)
+}
+
+// RandomUpperStr 随机字符串大写
+func RandomUpperStr(l uint32) string {
+	return strings.ToUpper(RandomLowerStr(l))
 }
